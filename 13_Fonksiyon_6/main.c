ed0de923f9c5e75cc9135a9e3f9e3e8a1aa931f9
HYUCE <huseyin@marmara.edu.tr>
2021-04-14 17:53:07 +0300

    update do while

--- 13_Fonksiyon_6/main.c
+++ 13_Fonksiyon_6/main.c
@@ -4,9 +4,14 @@ char Notu_Harfe_Donustur(int ogrenci_notu);
 
 int main() {
     int ogr_not;
-    printf("Lütfen notunuzu 0-100 arasında giriniz:");
-    scanf("%d",&ogr_not);
-    printf("Basarı Harf notunuz : %c dir", Notu_Harfe_Donustur(ogr_not) );
+    do {
+        printf("Lütfen notunuzu 0-100 arasında giriniz:");
+        scanf("%d",&ogr_not);
+        if (ogr_not>=0 && ogr_not <= 100)
+        printf("Basarı Harf notunuz : %c dir", Notu_Harfe_Donustur(ogr_not) );
+        else
+            printf("Girdiğiniz sayı 0-100 arasında bir değer olmali\n ");
+    } while (ogr_not<0 || ogr_not > 100);
     return 0;
 }
 
@@ -22,4 +27,3 @@ char Notu_Harfe_Donustur(int ogrenci_notu){
     if(ogrenci_notu>=90 && ogrenci_notu<=100)
         return 'A';
 }
-    
