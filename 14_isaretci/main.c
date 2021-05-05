d4265123b26cb190bb529b6beb7606f3c2c9912b
HYUCE <huseyin@marmara.edu.tr>
2021-05-05 17:34:12 +0300

    Update main.c

--- 14_isaretci/main.c
+++ 14_isaretci/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int *ptam, tam = 33;
-    ptam = &tam;
-    printf("tam: icerik = %d\n", tam);
-    printf("tam: adres = %p\n", &tam);
-    printf("ptam: icerik = %p\n", ptam);
-    printf("ptam: adres = %p\n", &ptam);
+    int tam = 33;
+    int *tAdresDegisken;
+
+    tAdresDegisken = &tam;
+
+    printf("Degiskenin Degeri :  %d\n", tam);
+    printf("Degiskenin Adresi :  %p\n", &tam);
+
+    printf("İşaretcinin Değeri :  %p\n", tAdresDegisken);
+
+    printf("tAdresDegisken Adresi :  %p", &tAdresDegisken);
+    
 
     return 0;
 }
