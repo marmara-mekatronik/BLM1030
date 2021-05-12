49c400833358c68bc0abe263ef46ccbd1e20987c
HYUCE <huseyin@marmara.edu.tr>
2021-05-12 11:57:55 +0300

    Update main.c

--- 15_dinamik_bellek_2/main.c
+++ 15_dinamik_bellek_2/main.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-
-    int matris[2][3];
-
+    //int matris[2][3];
     int **mat;
     int n,m;
     printf("satir sayisini giriniz : ");
@@ -16,14 +14,15 @@ int main() {
 
     if(mat==NULL){
         printf("%s:%d>Dizi için gerekli bellek ayrılamamıştır.\n", __FILE__, __LINE__);
-        return -1;
+        exit(0);
+    } else {
+
+        for (int i = 0; i < n; ++i) {
+            mat[i] = (int *) malloc(m * sizeof(int));
+        }
+        for (int k = 0; k < n; ++k) {
+            printf("%d satir bellek adresi : %p\n", k+1,mat[k]);
+        }
     }
-
-    for (int i = 0; i < n; ++i) {
-        mat[i] = (int*) malloc(m* sizeof(int ));
-    }
-
-
     return 0;
-
 }
