0a53b7f20205c232ffa205f665e940089fc19260
HYUCE <huseyin@marmara.edu.tr>
2021-05-12 18:34:08 +0300

    Update main.c

--- 15_dinamik_bellek_3/main.c
+++ 15_dinamik_bellek_3/main.c
@@ -9,7 +9,7 @@ int main() {
     printf("sütün sayisini giriniz : ");
     scanf("%d",&sutun);
 
-    int **matris = (int**) malloc(satir* sizeof(int*));
+    int **matris = (int**) malloc(satir* sizeof(int*)); // adresleri tutacağım bellek alanını oluştur.
     for (int i = 0; i < satir; ++i) {
         matris[i] = (int*) malloc(sutun * sizeof(int));
     }
