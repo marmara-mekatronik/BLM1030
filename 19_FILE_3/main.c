4c728743dc15a5e777be99cdf90b2147f926a5d5
HYUCE <huseyin@marmara.edu.tr>
2021-06-09 15:56:43 +0300

    Update main.c

--- 19_FILE_3/main.c
+++ 19_FILE_3/main.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 
 int main() {
-FILE* dosya = NULL;
+   FILE* dosya =NULL;
+   dosya = fopen("bilgi.txt","r");
+   if(dosya!=NULL){
+       printf("Dosya Okunuyor...\n");
+       printf("_________________________\n");
+       printf("_______DOSYA_ICERIGI_____\n");
+       printf("_________________________\n");
 
-dosya = fopen("bilgi.txt","r");
-if(dosya!=NULL){
-    printf("_________________________\n");
-    printf("_______DOSYA_ICERIGI_____\n");
-    printf("_________________________\n");
-    int ch = fgetc(dosya);
+       int ch = fgetc(dosya);
+       while(ch!=EOF){
+           putchar(ch);
+           ch = fgetc(dosya);
 
-    while(ch!=EOF){
-        putchar(ch);
-        ch= fgetc(dosya);
-    }
-    printf("\n");
-    fclose(dosya);
-    printf("_________________________\n");
-}
-// bilgi.txt
-//    Aşı, hastalıklara karşı bağışıklık sağlama amacı ile insan veya hayvan vücuduna verilen,
-//    zayıflatılmış hastalık virüsü, hastalık etkeninin parçaları veya salgıları ile
-//    oluşturulan çözeltidir
+       }
+       printf("\n");
+       printf("_________________________\n");
+
+   } else {
+       printf("Dosya açılamadı...\n");
+   }
 
 
     return 0;
 }
+
+//bilgi.txt dosya içeriği
+//Aşı, hastalıklara karşı bağışıklık sağlama amacı ile insan veya hayvan vücuduna verilen,
+//zayıflatılmış hastalık virüsü, hastalık etkeninin parçaları veya salgıları ile
+//oluşturulan çözeltidir.
