4650e5f9fe3efc452140b4ccaffec84046bd2e80
HYUCE <huseyin@marmara.edu.tr>
2021-06-09 18:33:03 +0300

    Update main.c

--- 19_FILE_6/main.c
+++ 19_FILE_6/main.c
@@ -14,34 +14,39 @@ int main() {
         float imdb;
         enum filmTur Tur;
     };
-    struct sinemaFilmi film = {"Titanic", 1997, 7.8, dram};
-    char *dosyaAdi = "film.db";
-    FILE *dosya = NULL;
-    dosya = fopen(dosyaAdi, "wb+");
-    if (dosya == NULL) {
+
+    struct sinemaFilmi film={"Titanic", 1997, 7.8, dram};
+    char *dosyaAdi ="film.db";
+
+    FILE* dosya=NULL;
+    dosya= fopen(dosyaAdi,"wb+");
+
+    if(dosya==NULL){
         printf("%s dosyasi oluşturulamadı/açılamadı\n", dosyaAdi);
         return 0;
-    } else {
-        fwrite(&film, sizeof(film), 1, dosya);
+    } else{
+        fwrite(&film, sizeof(film),1,dosya);
         fclose(dosya);
     }
 
     struct sinemaFilmi film2;
-
-    dosya = fopen(dosyaAdi, "rb+");
+    
+    dosya= fopen(dosyaAdi,"rb+");
+    
     if(dosya==NULL){
         printf("%s dosyasi oluşturulamadı/açılamadı\n", dosyaAdi);
         return 0;
     }else{
-        fread(&film2, sizeof(film), 1, dosya);
+        fread(&film2, sizeof(film),1,dosya);
         fclose(dosya);
         printf("->film.db Dosyasından okunan film bilgileri<-\n");
-        printf("____________________________________\n");
+        printf("__________________________________\n");
         puts(film2.ad);
         printf("%d\n",film2.yapimYili);
-        printf("%f\n",film2.imdb);
+        printf("%.2f\n",film2.imdb);
+        printf("%d\n",film2.Tur);
     }
-
 
     return 0;
 }
+
