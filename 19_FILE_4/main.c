5a031af4b8371f108eebd6f90fe0d770f4fb6a96
HYUCE <huseyin@marmara.edu.tr>
2021-06-09 16:38:56 +0300

    Update main.c

--- 19_FILE_4/main.c
+++ 19_FILE_4/main.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
 
 int main() {
-
-    char kaynakDosyaAdi[40];
-    char hedefDosyaAdi[40];
-
-    printf("Kopyalanacak dosyanın adını giriniz: ");
+ char kaynakDosyaAdi[40];
+ char hedefDosyaAdi[40];
+    printf("Kaynak Dosyanın Adını Giriniz: ");
     scanf("%s",kaynakDosyaAdi);
+
     FILE* kaynakDosya = NULL;
-    kaynakDosya = fopen(kaynakDosyaAdi,"r");
+    kaynakDosya= fopen(kaynakDosyaAdi,"r");
+
     if(kaynakDosya==NULL){
-        printf("Kopyalanack Dosya Bulunamadı! \n");
+        printf("İşlem yapılacak dosya bulunamadı\n");
         return 0;
-    }
-    printf("Yeni Dosya Adını Giriniz : ");
-    scanf("%s",hedefDosyaAdi);
-    FILE* hedefDosya = NULL;
-    if((hedefDosya=fopen(hedefDosyaAdi,"w")) !=NULL) {
-        int ch=fgetc(kaynakDosya);
-        while(ch!=EOF){
-            fputc(ch,hedefDosya);
-            ch= fgetc(kaynakDosya);
-        }
-        printf("\nKopyalama işlemi başarı ile tamamlandı...\n");
     }else{
-        printf("\n %s dosyası oluşturulurken bir hata oluştu", hedefDosya);
+        printf("Hedef Dosya Adını Giriniz:");
+        scanf("%s",hedefDosyaAdi);
+
+        FILE* hedefDosya = NULL;
+        hedefDosya = fopen(hedefDosyaAdi,"w");
+
+        if(hedefDosya !=NULL){
+            int ch= fgetc(kaynakDosya);
+            while(ch!=EOF){
+                fputc(ch,hedefDosya);
+                ch=fgetc(kaynakDosya);
+            }
+            printf("\nKopyalama işlemi başarı ile tamamlandı...\n");
+        }else{
+            printf("\n %s dosyası oluşturulurken bir hata oluştu", hedefDosya);
+        }
+        fclose(hedefDosya);
     }
-
     fclose(kaynakDosya);
-    fclose(hedefDosya);
-
     return 0;
 }
+
