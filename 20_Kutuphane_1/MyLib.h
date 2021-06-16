fc20a23b3dae05d54bef0d059ae5a4cd51691057
HYUCE <huseyin@marmara.edu.tr>
2021-06-16 11:07:46 +0300

    Create MyLib.h

---
+++ 20_Kutuphane_1/MyLib.h
@@ -0,0 +1,17 @@
+//
+// Created by hyuce on 16.06.2021.
+//
+
+
+#ifndef KUTUPHANE_MYLIB_H
+#define KUTUPHANE_MYLIB_H
+
+
+
+float ortalama(int x, int y);
+
+float DaireAlani(float r);
+float DDAlani(float Aci, float yaricap);
+
+#endif //KUTUPHANE_MYLIB_H
+
