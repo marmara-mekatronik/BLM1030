164f9be41e15b0e56f49733ac5411bae0c83cbce
HYUCE <huseyin@marmara.edu.tr>
2021-08-20 15:54:24 +0300

    Update main.c

--- 13_Fonksiyon_5/main.c
+++ 13_Fonksiyon_5/main.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 
-void kutu_ciz(int satir, int sutun);
-
+void tahta_ciz(int satir,int sutun);
 int main() {
-    kutu_ciz(10,50);
+    tahta_ciz(8,8);
     return 0;
 }
 
-void kutu_ciz(int satir, int sutun){
-    for (int i=satir; i > 0; i--) {
-        for (int j = sutun; j > 0 ; j--) {
-            printf("*");
+void tahta_ciz(int satir,int sutun){
+    int k=1;
+    for (int i = satir; i > 0 ; --i) {
+        for (int j = sutun; j > 0 ; --j) {
+            if(k==1){
+                printf(" [ ] ");
+            }
+                else{
+                printf(" [X] ");
+            }
+                k*=-1;
         }
-        printf("\n");
+
+        if(sutun%2==0){
+            k*=-1;
+        }
+        printf(" \n ");
     }
+
 }
