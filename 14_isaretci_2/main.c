c2c2c73690bffaca5be19a578ff76a800101dfdf
HYUCE <huseyin@marmara.edu.tr>
2021-08-27 10:51:23 +0300

    Update main.c

--- 14_isaretci_2/main.c
+++ 14_isaretci_2/main.c
@@ -5,10 +5,10 @@ int main() {
     int *pt, t=22;
     double *pg, g=10.5;
     pk = &k; pt = &t; pg= &g;
-    printf("Ilk Degerler: k= %c t= %d g= %f \n", k, t, g);
+    printf("Ilk Degerler: k= %c t= %d g= %.2f \n", k, t, g);
     printf("Onceki adresler: pk= %p pt= %p pg= %p \n", pk, pt, pg);
     k++;t--;g=g+10;
-    printf("Son Degerler: k= %c t= %d g= %f \n", k, t, g);
+    printf("Son Degerler: k= %c t= %d g= %.2f \n", k, t, g);
     pk++;
     pt--;
     pg=pg+10;
