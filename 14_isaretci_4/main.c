f3b686e53507f1daed93f1d5c19d2814e9614ffa
HYUCE <huseyin@marmara.edu.tr>
2021-08-27 13:34:03 +0300

    Update main.c

--- 14_isaretci_4/main.c
+++ 14_isaretci_4/main.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-/* fonksiyon deklarasyonu */
-double ortalama(int *p, int n);
+
+double ortalama(int *dizi, int n);
+
 int main() {
-    int dizi[5] = {10, 2, 3, 17, 50};
-    double ort;
-    printf("Dizi değişkeninin ilk elamanının pointer adresi : %p",dizi);
-    printf("Dizi değişkeninin ilk elamanının pointer adresi : %p",dizi[0]);
-    printf("\n--------------------\n");
-    /* pointera deger olarak diziyi atiyoruz */
-    ort = ortalama(dizi, 5);
-    /* dˆnd¸r¸len deger (return)  */
-    printf("ortalama deger %f\n", ort);
-    // yeni degeri yazdirma
+int dizi[5]={10,20,30,40,50};
+int diziBuyuklugu = sizeof (dizi)/ sizeof (int);
+
+    printf("Dizinin aritmetik ortalaması : %.2f", ortalama(dizi,diziBuyuklugu));
+
     return 0;
 }
-double ortalama(int *p, int n) {
-    int i, toplam = 0;
-    for (i = 0; i < n; ++i) {
-        toplam += p[i];
+
+double ortalama(int *dizi, int n){
+    int toplam=0;
+    for (int i = 0; i < n; ++i) {
+        toplam+=*(dizi+i);
+        //toplam=toplam+(*dizi)
+        //dizi++;
     }
-    return toplam / n;
+    return toplam/n;
 }
