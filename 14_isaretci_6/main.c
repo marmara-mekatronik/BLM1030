fc0cda7ec1056a385e2cd2f77923c80fef855aea
HYUCE <huseyin@marmara.edu.tr>
2021-08-27 15:37:44 +0300

    Update main.c

--- 14_isaretci_6/main.c
+++ 14_isaretci_6/main.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
-int *minBul( int a[],int boyut){
-	int ekd=a[0];
-	int *eka=&a[0];
-	int i;
-	for (i=1;i<boyut;i++){
-		if(a[i]<ekd){
-			ekd=a[i];
-			eka=&a[i];
-		}
-	}
-	return eka;
+int *minBul(int dizi[], int boyut);
 
+int main() {
+    int dizi[17]={25,8,9,6,15,20,35,40,50,11,2,13};
+    int diziUzunlugu= sizeof(dizi)/sizeof(int);
+
+    int *ptr;
+    ptr= minBul(dizi,diziUzunlugu);
+
+    printf("En kucuk degerin adresi: %p\n",ptr);
+    printf("En Kucuk deger: %d\n", *ptr);
+    printf("En kucuk degerin indis bilgisi %p\n",(ptr-&dizi[0]));
+    printf("En kucuk degerin indis bilgisi %d",(int)(ptr-&dizi[0]));
+    return 0;
 }
-int main(){
-	int x[5]={25,8,9,6,15};
-	int *p;
-	p=minBul(x,5);
 
-	printf("En kucuk deger =%d\n",*p);
-	printf("En kucuk degerin adresi =%p\n",p);
-	printf("En kucuk degerin indis bilgisi  =%d\n",(int)(p-&x[0]));
- }
+int *minBul(int dizi[], int boyut){
+ int EnKucukDeger=dizi[0];
+ int *EnKucukAdres=&dizi[0]; //int *EKA=dizi;
+    for (int i = 1; i < boyut; ++i) {
+        if(dizi[i]<EnKucukDeger){
+            EnKucukDeger=dizi[i];
+            EnKucukAdres=&dizi[i];
+        }
+    }
+    return EnKucukAdres;
+}
