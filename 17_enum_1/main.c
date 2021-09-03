6088b8e296a59e54b03464fbcf353ef86430e44f
HYUCE <huseyin@marmara.edu.tr>
2021-09-03 15:03:20 +0300

    Update main.c

--- 17_enum_1/main.c
+++ 17_enum_1/main.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+    enum KullaniciSeviyeleri{
+        misafir=1, ogrenci=2,
+        akademisyen, admin
+    }KSeviye;
 
-    enum KullaniciSeviyeri {
-        misafir = 1,
-        ogrenci = 2,
-        akademisyen = 3,
-        admin = 4
-    };
-    // int degisken_ismi;
-    enum KullaniciSeviyeri seviye;
-    printf("Kullanici seviyenizi 1-4 arasinda giriniz : ");
-    scanf("%d", &seviye);
-    switch (seviye) {
+    char *Sifreler[]={"1234","5678","9011","4321"};
+    char *sifre[4];
+
+    printf("Lütfen Şifrenizi giriniz : ");
+    scanf("%s", sifre);
+
+if(strcmp(Sifreler[0],sifre)==0){KSeviye=misafir;}
+else if (strcmp(Sifreler[1],sifre)==0){KSeviye=ogrenci;}
+else if (strcmp(Sifreler[2],sifre)==0){KSeviye=akademisyen;}
+else if (strcmp(Sifreler[3],sifre)==0){KSeviye=admin;}
+
+    switch (KSeviye) {
         case misafir:
-            printf("Yetki seviyeniz misafirdir\n");
-            break;
+            printf("Yetki Seviyeniz misafirdir..."); break;
         case ogrenci:
-            printf("Yetki seviyeniz ogrencidir\n");
-            break;
+            printf("Yetki Seviyeniz ogrencidir..."); break;
         case akademisyen:
-            printf("Yetki seviyeniz Akademisyendir\n");
-            break;
+            printf("Yetki Seviyeniz akademisyendir..."); break;
         case admin:
-            printf("Yetki seviyeniz admindir\n");
-            break;
+            printf("Yetki Seviyeniz admindir...."); break;
         default:
-            printf("Girdiğiniz değer geçersizdir");
-            break;
+            printf("Girdiginiz deger gecersizdir..."); break;
     }
     return 0;
 }
+
