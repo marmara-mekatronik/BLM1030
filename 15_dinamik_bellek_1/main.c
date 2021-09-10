a8da49ffaa66154a28d717fc774dc00b5a506c0c
HYUCE <huseyin@marmara.edu.tr>
2021-09-10 10:33:59 +0300

    Update main.c

--- 15_dinamik_bellek_1/main.c
+++ 15_dinamik_bellek_1/main.c
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 
 int main() {
-
     int n;
     int *list;
 
-    printf("Kaç sayı üzerinde işlem yapılacak?");
+    printf("Kaç sayı üzerinde işlem yapacaksınız?");
     scanf("%d", &n);
 
-    list = (int*)malloc(n*sizeof(int));
-    if(list==NULL)
-        printf("%s:%d>Dizi için gerekli bellek ayrılamamıştır.\n", __FILE__, __LINE__);
+    list = (int *) malloc(n * sizeof(int));
+    if (list == NULL)
+        printf("Tarih : %s - %s \n Kaynak Kodunuz : %s:\n %d nolu satir -> İşlem için gerekli bellek ayrılamamıştır.\n",
+               __DATE__, __TIME__, __FILE__, __LINE__);
     else
-        printf("istemiş olduğunuz bellek ayrıldı. Başlangıç adresi :%p", list);
+        printf("İstemiş olduğunuz bellek ayrıldı. Bellek başlangıç adresi: %p", list);
 
 
     return 0;
-
 }
