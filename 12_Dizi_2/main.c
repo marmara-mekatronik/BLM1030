2d516d14f73f7a2c98cf1498cbe6d79505227f8d
HYUCE <huseyin@marmara.edu.tr>
2023-03-28 11:52:48 +0300

    Update main.c

--- 12_Dizi_2/main.c
+++ 12_Dizi_2/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main() {
     char c_dizi1[9] = {'K', 'a', 'r', 'a', 'k', 't', 'e', 'r', '\0'};
     char c_dizi2[9] = "Karakter";
@@ -9,16 +8,29 @@ int main() {
     char c_dizi4[] = "Diziler";
 
     int i_dizi1[10];
-    int i_dizi2[2][3][4];
+    int i_dizi2[2][3][4]; // 3 boyutlu dizi KUP
     int i_dizi3[] = {1, 2, 3, 4, 5, 6};
 
-    for (int i = 0; i < 10; ++i) {
-        i_dizi1[i] = i * i;
-        printf("%d \t", i_dizi1[i]);
+    printf("%s - %s \n", c_dizi1, c_dizi2);
 
+    strcpy(c_dizi3[0], "Karekter Dizisi 1");
+    strcpy(c_dizi3[1], "karekter Dizisi 2");
+
+    printf("%s\n", c_dizi3[0]);
+    printf("%s\n", c_dizi3[1]);
+
+    for (int i = 0; i < sizeof(c_dizi4) / sizeof(char) - 1; ++i) {
+        printf("%c", c_dizi4[i]);
     }
 
-    printf("\n%s - %s ", c_dizi1, c_dizi2);
+    printf("\n");
+
+    for (int i = 0; i < sizeof(i_dizi1) / sizeof(int); ++i) {
+        i_dizi1[i] = i * i;
+        printf("%d \t", i_dizi1[i]);
+    }
+    printf("\n");
+    printf("---------\n");
 
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 3; ++j) {
@@ -31,24 +43,10 @@ int main() {
         printf("\n");
     }
 
-    strcpy(c_dizi3[0], "Karekter Dizisi 1");
-    strcpy(c_dizi3[1], "Karekter Dizisi 2");
-
-    printf("%s\n", c_dizi3[0]);
-    printf("%s\n", c_dizi3[1]);
-
-    for (int i = 0; i < sizeof(c_dizi4) / sizeof(char); ++i) {
-        printf("%c", c_dizi4[i]);
-    }
-    printf("\n");
-    puts(c_dizi4);
-
-
     for (int i = 0; i < sizeof(i_dizi3) / sizeof(int); ++i) {
-        printf("%d\t", i_dizi3[i]);
+        printf("%d \t", i_dizi3[i]);
     }
+
 
     return 0;
 }
-
-
