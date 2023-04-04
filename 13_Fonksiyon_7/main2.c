e9701a2a52ce7d8bfbe9ff6aede3632ca1a278ef
HYUCE <huseyin@marmara.edu.tr>
2023-04-04 11:53:45 +0300

    Update main2.c

--- 13_Fonksiyon_7/main2.c
+++ 13_Fonksiyon_7/main2.c
@@ -1,42 +1,37 @@
 #include <stdio.h>
 #include <limits.h>
 
-int sonuc_ulong_max_asar_mi(unsigned int sayi);
+int sonuc_ulong_max_asar_mi(unsigned short int sayi);
 
-unsigned long long int faktoriyel_hesaplama(unsigned int sayi);
+unsigned long long int faktoreyel_hesaplama(unsigned short int sayi);
 
 int main() {
-    for (int i = 0; i < USHRT_MAX; ++i) {
+    for (unsigned short int i = 1; i < USHRT_MAX; ++i) {
         if (sonuc_ulong_max_asar_mi(i)) {
             break;
         } else {
-            printf("%u! = %llu\n", i, faktoriyel_hesaplama(i));
+            printf("%hu! = %llu\n", i, faktoreyel_hesaplama(i));
         }
     }
     return 0;
 }
-
-// faktoriyel hesaplayan fonksiyon
-unsigned long long int faktoriyel_hesaplama(unsigned int sayi) {
+unsigned long long int faktoreyel_hesaplama(unsigned short int sayi) {
     if (sayi == 0 || sayi == 1)
         return 1;
-    return (sayi * faktoriyel_hesaplama(sayi - 1));
-};
-
+    return (sayi * faktoreyel_hesaplama(sayi - 1));
+}
 // Faktoriyel sonucunun ULLONG_MAX'i aşıp aşmadığını kontrol
 //eden fonksiyon
 // ULLONG_MAX = 18446744073709551615
-// 21! = 14197454024290336768
 
-int sonuc_ulong_max_asar_mi(unsigned int sayi) {
+int sonuc_ulong_max_asar_mi(unsigned short int sayi) {
     unsigned long long int sonuc = 1;
-    for (unsigned int i = 1; i < sayi; ++i) {
+    for (unsigned short int i = 1; i < sayi; ++i) {
         if (ULLONG_MAX / i < sonuc) {
-            return 1; // sonuç ULLONG_MAX'i aşarsa
+            return 1; // sonuç ULLONG_MAX'aşarsa
+        } else {
+            sonuc *= i; // sonuç ULLONG_MAX'ı aşmaz İSE
         }
-        sonuc *= i; // Sonuç UULONG_MAX'i aşmıyor
     }
     return 0;
 }
-
-
