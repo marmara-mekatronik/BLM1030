6d282f6b18366227be1db3c2896031cf231717cd
HYUCE <huseyin@marmara.edu.tr>
2023-04-17 10:47:28 +0300

    Update main5.c

--- 13_Fonksiyon_8/main5.c
+++ 13_Fonksiyon_8/main5.c
@@ -7,18 +7,19 @@ int main(int argc, char *argv[]) {
         printf("Lütfen en az üç argüman giriniz: sayı1 işlem sayı2\n");
         return 1;
     }
-    switch (*argv[1]) {
-        case '*':
-            printf("%.2f", atof(argv[2]) * atof(argv[3]));
+    switch (*argv[2]) {
+        case 'x':
+            printf("%.2f", atof(argv[1]) * atof(argv[3]));
+            // \* kullan
             break;
         case '/':
-            printf("%.2f", atof(argv[2]) / atof(argv[3]));
+            printf("%.2f", atof(argv[1]) / atof(argv[3]));
             break;
         case '+':
-            printf("%.2f", atof(argv[2]) + atof(argv[3]));
+            printf("%.2f", atof(argv[1]) + atof(argv[3]));
             break;
         case '-':
-            printf("%.2f", atof(argv[2]) - atof(argv[3]));
+            printf("%.2f", atof(argv[1]) - atof(argv[3]));
             break;
         default:
             printf("Doğru işlem giriniz: +, -, * veya /\n");
