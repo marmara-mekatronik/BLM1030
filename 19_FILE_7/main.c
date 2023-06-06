fecc190a3064fece849b8888b9eba341f74016eb
HYUCE <huseyin@marmara.edu.tr>
2023-06-06 12:21:05 +0300

    Update main.c
    güncelleme

--- 19_FILE_7/main.c
+++ 19_FILE_7/main.c
@@ -16,24 +16,33 @@ typedef struct {
 
 int main() {
     SinemaFilmi Film[] = {
-            {"Titanic", 1997, 7.8, dram},
-            {"Togo", 2019, 8.2, dram},
+            {"Titanic",    1997, 7.8, dram},
+            {"Togo",       2019, 8.2, gerilim},
             {"Green Book", 2018, 8.3, komedi},
-            {"Alien", 1979, 8.1, korku}
+            {"Alien",      1979, 8.1, korku}
     };
+    int elemanSayisi = sizeof(Film) / sizeof(SinemaFilmi);
+    printf("%d adet film var\n", elemanSayisi);
+
+    for (int i = 0; i < elemanSayisi; ++i) {
+        printf("Film Adi: %s \n",Film[i].ad);
+        printf("Film Yapim Yili:%d\n",Film[i].yapimYili);
+        printf("Film IMDB Puani:%.2f\n",Film[i].imdb);
+        printf("Film Turu:%d\n",Film[i].tur);
+        printf("____________________________________\n");
+    }
+
     char *dosyaAdi = "film.db";
     FILE *Dosya = fopen(dosyaAdi, "wb+");
-    if (Dosya == NULL) {
+    if(Dosya==NULL){
         printf("%s dosyasi olusturulamadi/acilamadi\n", dosyaAdi);
         exit(1);
     }
-    for (int i = 0; i < sizeof(Film)/ sizeof(SinemaFilmi); ++i) {
+    for (int i = 0; i < elemanSayisi; ++i) {
         fwrite(&Film[i], sizeof(Film[i]), 1, Dosya);
     }
     fclose(Dosya);
-
     SinemaFilmi okunanFilm;
-
     Dosya = fopen(dosyaAdi, "rb+");
     if (Dosya == NULL) {
         printf("%s dosyasi okunamadi !\n", dosyaAdi);
@@ -41,15 +50,16 @@ int main() {
     }
     printf("->film.db Dosyasindan okunan film bilgileri<-\n");
     printf("____________________________________\n");
-    for (int i = 0; i <  sizeof(Film)/ sizeof(SinemaFilmi); ++i) {
-        fread(&okunanFilm, sizeof(Film[i]),1,Dosya);
+    for (int i = 0; i < elemanSayisi; ++i) {
+        fread(&okunanFilm, sizeof(okunanFilm),1,Dosya);
         printf("Film Adi: %s \n",okunanFilm.ad);
         printf("Film Yapim Yili:%d\n",okunanFilm.yapimYili);
         printf("Film IMDB Puani:%.2f\n",okunanFilm.imdb);
         printf("Film Turu:%d\n",okunanFilm.tur);
-        printf("Dosya Konum Bilgisi: %ld . byte \n", ftell(Dosya));
+        printf("Dosya Konum Bilgisi : %ld. byte\n", ftell(Dosya));
         printf("____________________________________\n");
     }
     fclose(Dosya);
+
     return 0;
 }
