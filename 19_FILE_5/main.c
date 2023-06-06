caab95e453dab73a911bf90f1728119407a04ed7
HYUCE <huseyin@marmara.edu.tr>
2023-06-06 11:13:44 +0300

    Update main.c
    Bir kaç hata düzeltildi.

--- 19_FILE_5/main.c
+++ 19_FILE_5/main.c
@@ -18,33 +18,34 @@ int main() {
         fscanf(parametreDosyasi, "%d %d %d %d %c", &kNo, &AktifPasif, &Sicaklik,
                &Kademe, &SogukSicak);
 
+        fclose(parametreDosyasi);
+
         FILE *sonucDosyasi = NULL;
         sonucDosyasi = fopen(sonucDosyaAdi, "w");
         if (sonucDosyasi != NULL) {
             switch (AktifPasif) {
                 case 0:
                     AktifPasif = 1;
+                    fprintf(sonucDosyasi, "%d nolu klima Açıkken Kapatıldı.", kNo);
                     break;
                 case 1:
                     AktifPasif = 0;
+                    fprintf(sonucDosyasi, "%d nolu klima Kapalıyken Açıldı."
+                                          "Sıcaklık: %d , Kademe: %d , "
+                                          "Soğuk/Sıcak: %c olarak "
+                                          "SET edildi\n", kNo, Sicaklik, Kademe, SogukSicak);
                     break;
                 default:
                     printf("Yanlış Parametre Bilgisi");
+                    fclose(sonucDosyasi);
                     return 0;
             }
-        }
-        if (AktifPasif == 0) {
-            fprintf(sonucDosyasi, "%d nolu klima Açıkken Kapatıldı.", kNo);
+            printf("\nYapılan işlemler %s dosyasına yazıldı", sonucDosyaAdi);
+            fclose(sonucDosyasi);
         } else {
-            fprintf(sonucDosyasi, "%d nolu klima Kapalıyken Açıldı."
-                                  "Sıcaklık: %d , Kademe: %d , "
-                                  "Soğuk/Sıcak: %c olarak "
-                                  "SET edildi\n", kNo, Sicaklik, Kademe, SogukSicak);
+            printf("sonuc dosyası oluşturulamadı/açılamadı\n");
+            return 0;
         }
-        printf("\nYapılan işlemler %s dosyasına yazıldı", sonucDosyaAdi);
-        fclose(sonucDosyasi);
     }
-    fclose(parametreDosyasi);
     return 0;
 }
-
