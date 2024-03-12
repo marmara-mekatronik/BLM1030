297fd77ff78dc945295beda713263dd99d5952be
HYUCE <huseyin@marmara.edu.tr>
2024-03-12 16:09:38 +0300

    Update main.c

--- 6_2_For_Dongusu/main.c
+++ 6_2_For_Dongusu/main.c
@@ -1,28 +1,22 @@
 #include<stdio.h>
 
 int main (){
-	int sayac, baslangic, bitis;
-	int toplam = 0 ;
-	
-	printf("Baslangic sayisini giriniz : ");
-	scanf("%d", &baslangic);
-	
-	printf("Bitis sayisini giriniz : ");
-	scanf("%d", &bitis);
-	
-	if(baslangic<bitis){
+    int sayac, baslangic, bitis;
+    int toplam = 0 ;
 
-	for(sayac = baslangic; sayac<= bitis ;++sayac){
-		toplam +=sayac;	
-	}
-	
-	printf("Toplam : %d\n",toplam);
-	
-	
-		}
-		
-	else 
-		printf("Lutfen baslangic degerini bitisten dusuk giriniz.");
-		
-	return 0 ;
+    printf("Baslangic sayisini giriniz : ");
+    scanf("%d", &baslangic);
+
+    printf("Bitis sayisini giriniz : ");
+    scanf("%d", &bitis);
+
+    if(baslangic<bitis){
+        for(sayac = baslangic; sayac<= bitis ;++sayac){
+            toplam +=sayac;
+        }
+        printf("Toplam : %d\n",toplam);
+    }else{
+        printf("Lutfen baslangic degerini bitisten dusuk giriniz.");
+}
+return 0 ;
 }
