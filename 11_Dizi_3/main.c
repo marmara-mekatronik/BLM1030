e160de5d73ec14fc67414f870aaf7037dc62a573
furkankuyumcu54 <kuyumcufurkan05@gmail.com>
2024-03-21 23:01:19 +0300

    Değiştirilen koda Yorumlar eklendi.

--- 11_Dizi_3/main.c
+++ 11_Dizi_3/main.c
@@ -10,15 +10,15 @@ int main() {
     printf("-----------------\n");
     for (int i = 0; i < 8; ++i) {
 
-        printf("|");
+        printf("|"); // Satır Başı Çizgisi
         for (int j = 0; j < 8; ++j) {
             if ((i + j) % 2 == 0)
-                satranc[i][j] = '#';
+                satranc[i][j] = '#'; // Siyah kareleri belirliyoruz.
             else
-                satranc[i][j] = ' ';
+                satranc[i][j] = ' '; // Beyaz Kareleri belirliyoruz.
 
-            printf("%c",satranc[i][j]);
-            printf("|");
+            printf("%c",satranc[i][j]); // Belirlediğimiz kareleri yazdırıyoruz.
+            printf("|"); // Satır Sonu Çizgisi
         }
         printf("\n");
     }
