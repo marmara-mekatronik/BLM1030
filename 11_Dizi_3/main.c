75cf85678cb039887abcf4c6e7ba611b72ec6876
gvatansever24 <155946935+gvatansever24@users.noreply.github.com>
2024-03-22 11:19:25 +0300

    Update main.c
    Kod Tek Döngüye İndirgendi.
    Output Görüntüsü Değiştirildi/Güzelleştirildi.
    Koda Notlar Eklendi.

--- 11_Dizi_3/main.c
+++ 11_Dizi_3/main.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
-#include <string.h>
-
-#define SATIR 8
-#define SUTUN 8
-
-int main() {
-    char satranc[SATIR][SUTUN];
-
-    for (int i = 0; i < SATIR; ++i) {
-        for (int j = 0; j < SUTUN; ++j) {
-            satranc[i][j] = ' ';
-
-        }
-    }
-
-    for (int i = 0; i < SATIR; ++i) {
-        for (int j = 0; j < SUTUN; ++j) {
-            if ((i + j) % 2 == 0) {
-                satranc[i][j] = '#';
+int main()
+{
+    // char dizisi oluştur
+    // i+j % 2 == 0 ise X koy != 0 ise ' ' olsun
+    char tahta[8][8];
+    int i,j;
+    printf("  --------------------------\n");
+    for ( i = 0; i < 8; i++)
+    {
+        printf("%d |",i+1); // Kaçıncı Sütünda Olduğumuzun Bilgisi.
+        for ( j = 0; j < 8; j++)
+        {
+            if ((i+j)%2==0)
+            {
+                printf("|X|"); // Siyah Kareleri Belirledik.
             }
+            else
+            printf("   "); // Boş Kareler.
         }
-    }
-
-    for (int i = 0; i < SATIR; ++i) {
-        for (int j = 0; j < SUTUN; ++j) {
-            printf("%c",satranc[i][j]);
-        }
+        printf("|");
         printf("\n");
     }
-
+        printf("  --------------------------\n");
+        printf("    A  B  C  D  E  F  G  H ");
     return 0;
 }
-
-
