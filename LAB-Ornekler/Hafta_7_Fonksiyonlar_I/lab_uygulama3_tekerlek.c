15029963c849738fcfe9681a65e558f0648402fb
Deniz Leloğlu <denizleloglu@marun.edu.tr>
2026-04-21 15:31:50 +0300

    yarıçap limit testleri
    negatif uzunluk ve sonsuz çıktılar engellendi

--- LAB-Ornekler/Hafta_7_Fonksiyonlar_I/lab_uygulama3_tekerlek.c
+++ LAB-Ornekler/Hafta_7_Fonksiyonlar_I/lab_uygulama3_tekerlek.c
@@ -7,11 +7,15 @@ float cevreHesapla(float r);     // Prototip
 int main() {
     float yaricap;
     printf("Tekerlek yaricapi (cm): ");
-    if (scanf("%f", &yaricap) != 1) {
+    if (scanf("%f", &yaricap) != 1  || yaricap <0) {    // Yarıçapın 0'dan küçük olma durumu kaldırıldı
         printf("Hata: Gecersiz giris.\n");
+        return 1; }
+    if (yaricap > 1000000) {
+        printf("Hata: Girdiginiz deger cok buyuk.\n"); // Float'ın sınırlarını aşan boyutta sayılarda infinite çıktı engellendi
         return 1;
     }
-    
+
+
     printf("Alan  : %.2f cm2\n", alanHesapla(yaricap));
     printf("Cevre : %.2f cm\n", cevreHesapla(yaricap));
     printf("1 tur : %.2f cm yol alir.\n", cevreHesapla(yaricap));
